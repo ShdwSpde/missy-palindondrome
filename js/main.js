482d5acd9abe881dd89900d62cd17ebd158af1c9
Mike Spade <mikespadeny@gmail.com>
2022-03-31 12:00:35 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,11 +8,11 @@ const palindromeCheck = str => console.log(str === str.split('').reverse().join(
 
 const audio = new Audio('Missy.mp3');
 
-const missyPalindrome = function(str) {
-  if (palindromeCheck() = 'true'){
+const missyPalindrome() = function(str) {
+  if (palindromeCheck(str) === true){
     document.querySelector('body').style.backgroundImage = "url(https://c.tenor.com/EXzrW3-GMGoAAAAC/missy-elliot-work-it.gif)"
     audio.play();
   }
 }
 
-document.getElementById("submit").onclick = missyPalindrome(document.getElementById("workIt"))
+document.getElementById("submit").onclick = missyPalindrome(document.getElementById("workIt").value)
